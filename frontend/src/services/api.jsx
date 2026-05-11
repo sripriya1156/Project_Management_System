71abb6bda08c63f79a453c2e679e8dd6a82e501d
Vishnu Sri Priya Kala <vishnusripriyk@gmail.com>
2026-05-11 16:46:23 +0530

    fix: update API service config

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://project-management-system-rcx3.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
